8f55578dcfd2e842cd90f4d4fbd7d29c7c6556d6
Nicolas Bondoux <8930930+Nsbx@users.noreply.github.com>
2022-02-19 17:03:35 +0100

    Add missing events to MeteoFrance & update French translation (#76)

--- src/integrations/meteofrance.js
+++ src/integrations/meteofrance.js
@@ -5,13 +5,18 @@ const STATE_YELLOW = 'Jaune';
 const STATE_ORANGE = 'Orange';
 const STATE_RED    = 'Rouge';
 
-const EVENT_WIND                     = 'Vent violent';
-const EVENT_SNOW_ICE                 = 'Neige-verglas';
-const EVENT_THUNDERSTORMS            = 'Orages';
-const EVENT_FLOOD                    = 'Inondation';
-const EVENT_RAIN_FLOOD               = 'Pluie-inondation';
+// The list of event alert is located here
+// https://github.com/hacf-fr/meteofrance-api/blob/master/src/meteofrance_api/const.py
+
+const EVENT_WIND             = 'Vent violent';
+const EVENT_RAIN_FLOOD       = 'Pluie-inondation';
+const EVENT_THUNDERSTORMS    = 'Orages';
+const EVENT_FLOOD            = 'Inondation';
+const EVENT_SNOW_ICE         = 'Neige-verglas';
 const EVENT_HIGH_TEMPERATURE = 'Canicule';
 const EVENT_LOW_TEMPERATURE  = 'Grand-froid';
+const EVENT_AVALANCHES       = 'Avalanches';
+const EVENT_COASTAL          = 'Vagues-submersion';
 
 export class MeteoFranceIntegration
 {
@@ -32,13 +37,15 @@ export class MeteoFranceIntegration
 	static getEventsTypes()
 	{
 		return {
-			[EVENT_WIND]:          			  Data.getEventByName('Wind'),
-			[EVENT_SNOW_ICE]:      			  Data.getEventByName('Snow/Ice'),
-			[EVENT_THUNDERSTORMS]: 			  Data.getEventByName('Thunderstorms'),
-			[EVENT_FLOOD]:         			  Data.getEventByName('Flooding'),
-			[EVENT_RAIN_FLOOD]:    			  Data.getEventByName('Rain-Flood'),
+			[EVENT_WIND]:             Data.getEventByName('Wind'),
+			[EVENT_RAIN_FLOOD]:       Data.getEventByName('Rain-Flood'),
+			[EVENT_THUNDERSTORMS]:    Data.getEventByName('Thunderstorms'),
+			[EVENT_FLOOD]:            Data.getEventByName('Flooding'),
+			[EVENT_SNOW_ICE]:         Data.getEventByName('Snow/Ice'),
 			[EVENT_HIGH_TEMPERATURE]: Data.getEventByName('High temperature'),
 			[EVENT_LOW_TEMPERATURE]:  Data.getEventByName('Low temperature'),
+			[EVENT_AVALANCHES]:       Data.getEventByName('Avalanches'),
+			[EVENT_COASTAL]:          Data.getEventByName('Coastal Event'),
 		};
 	}
 
