33cab9e37cb3d5187a12c51cbd7a13d7d27a35b4
Bartosz Dokurno <bartusekcraft@gmail.com>
2023-10-14 14:44:09 +0200

    Update dependencies and add eslint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,26 +1,36 @@
 module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+		node: false,
+	},
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:prettier/recommended',
+	],
+	ignorePatterns: ['*.json'],
 	parser: '@typescript-eslint/parser',
 	parserOptions: {
 		ecmaVersion: 2020,
-		sourceType: 'module'
+		sourceType: 'module',
 	},
-	extends: [
-		'plugin:@typescript-eslint/recommended'
-	],
+	plugins: ['@typescript-eslint', 'prettier'],
 	rules: {
-		'brace-style': ['error', 'stroustrup', { 'allowSingleLine': true }],
-		'quotes': ['error', 'single'],
-		'no-trailing-spaces': ['error'],
-		'eol-last': ['error', 'always'],
-		'curly': ['error', 'multi-line', 'consistent'],
-		'indent': ['error', 'tab', { 'SwitchCase': 1, 'ignoredNodes': ['TemplateLiteral *'] }],
-		'semi': ['error', 'always'],
-		'no-multiple-empty-lines': ['error', { 'max': 1}],
-		'comma-dangle': ['error', 'never'],
 		'@typescript-eslint/no-non-null-assertion': 'off',
 		'@typescript-eslint/no-explicit-any': 'off',
 		'@typescript-eslint/no-this-alias': 'off',
 		'@typescript-eslint/ban-ts-comment': 'off',
-		'no-console': 'error'
-	}
+		'no-console': 'error',
+		'prettier/prettier': [
+			'error',
+			{
+				endOfLine: 'auto',
+				useTabs: true,
+				singleQuote: true,
+				printWidth: 100,
+				singleAttributePerLine: true,
+			},
+		],
+	},
 };
