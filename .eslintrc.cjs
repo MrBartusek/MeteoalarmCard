43df4c0536aa8c4c80ac9884a13af2e860c21a5f
Bartosz Dokurno <bartusekcraft@gmail.com>
2023-10-14 14:49:10 +0200

    Fix indent

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,37 +1,37 @@
 module.exports = {
-	env: {
-		browser: true,
-		es2021: true,
-		node: false,
-	},
-	extends: [
-		'eslint:recommended',
-		'plugin:@typescript-eslint/recommended',
-		'plugin:prettier/recommended',
-	],
-	ignorePatterns: ['*.json'],
-	parser: '@typescript-eslint/parser',
-	parserOptions: {
-		ecmaVersion: 2020,
-		sourceType: 'module',
-	},
-	plugins: ['@typescript-eslint', 'prettier'],
-	rules: {
-		'@typescript-eslint/no-non-null-assertion': 'off',
-		'@typescript-eslint/no-explicit-any': 'off',
-		'@typescript-eslint/no-this-alias': 'off',
-		'@typescript-eslint/ban-ts-comment': 'off',
+    env: {
+        browser: true,
+        es2021: true,
+        node: false,
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:prettier/recommended',
+    ],
+    ignorePatterns: ['*.json'],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+    },
+    plugins: ['@typescript-eslint', 'prettier'],
+    rules: {
+        '@typescript-eslint/no-non-null-assertion': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/no-this-alias': 'off',
+        '@typescript-eslint/ban-ts-comment': 'off',
         '@typescript-eslint/no-unsafe-declaration-merging': 'off',
-		'no-console': 'error',
-		'prettier/prettier': [
-			'error',
-			{
-				endOfLine: 'auto',
-				useTabs: true,
-				singleQuote: true,
-				printWidth: 100,
-				singleAttributePerLine: true,
-			},
-		],
-	},
+        'no-console': 'error',
+        'prettier/prettier': [
+            'error',
+            {
+                endOfLine: 'auto',
+                useTabs: true,
+                singleQuote: true,
+                printWidth: 100,
+                singleAttributePerLine: true,
+            },
+        ],
+    },
 };
