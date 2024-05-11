ace53c8a0feba9c97cdb747572a4ae301e50d5da
Bartosz Dokurno <bartusekcraft@gmail.com>
2024-05-11 14:49:48 +0200

    Map `Avis D'` to Advisory

--- src/integrations/env_canada.ts
+++ src/integrations/env_canada.ts
@@ -209,7 +209,7 @@ export default class EnvironmentCanada implements MeteoalarmIntegration {
 			{
 				type: EnvCanadaEntityType.Advisory,
 				en: ['Advisory'],
-				fr: ['Avis De', 'Avis'],
+				fr: ['Avis De', "Avis D'", 'Avis'],
 			},
 		];
 	}
