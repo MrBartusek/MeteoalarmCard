2b0bf04fad62b503cfcb6cea661136967f8dd47c
MrBartusek <bartusekcraft@gmail.com>
2026-08-22 20:38:24 +0200

    Don't cache canvas

--- src/measure-text.ts
+++ src/measure-text.ts
@@ -1,5 +1,3 @@
-let context: CanvasRenderingContext2D | undefined;
-
 /**
  * Uses canvas.measureText to compute and return the width of the given text of given font in pixels.
  *
@@ -9,7 +7,8 @@ let context: CanvasRenderingContext2D | undefined;
  * @see https://stackoverflow.com/questions/118241/calculate-text-width-with-javascript/21015393#21015393
  */
 export function getTextWidth(text: string, font: string): number {
-	context ??= document.createElement('canvas').getContext('2d')!;
+	const canvas = document.createElement('canvas');
+	const context = canvas.getContext('2d')!;
 	context.font = font;
 	const metrics = context.measureText(text);
 	return metrics.width;
